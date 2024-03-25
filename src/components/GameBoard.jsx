72e92a2a490d37cbc738bf1419338ac2850c645b
Martin Fahlström <makrill88@gmail.com>
2024-03-25 13:30:31 +0100

    removed Newgame component

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -8,6 +8,8 @@ export default function GameBoard() {
 	
 	function shuffleArray(array) {
 		const shuffledArray = [...array];
+
+		//Fisher-Yates shuffle
 		for (let i = shuffledArray.length - 1; i > 0; i--){
 			const j = Math.floor(Math.random() * (i + 1));
 			[shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
