c161955c468895ec473aef1cba0fbcd29cacaa95
Martin Fahlström <makrill88@gmail.com>
2024-03-25 12:46:40 +0100

    shuffle function

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -3,8 +3,26 @@ import Tile from './Tile'
 
 export default function GameBoard() {
 
-	
+	/* 
+   The 'numbers' array is generated using Array.from() to create an array containing numbers from 1 to 16.
+	*/
 	const numbers = Array.from({ length: 16}, (_, index) => index +1)
+	// 	Alternative: const numbers = [];
+	// 	for (let i = 1; i <= 16; i++) {
+	// 	numbers.push(i);
+	//   }
+
+	shuffleArray(numbers);
+	console.log(numbers);
+
+
+	function shuffleArray(array) {
+		for (let i = array.length -1; i > 0; i--){
+			const j = Math.floor(Math.random() * (i + 1));
+			[array[i], array[j]] = [array[j], array[i]];
+		}
+		return array;
+	}
 
 	function handleTileMove(params) {
 		
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,13 @@ export default function Header() {
   return (
 	<header>
 		<h1>15-Puzzle</h1>
+		{/* TODO: 
+		<p>Win/loose: </p>
+		<p>timer: </p>
+		<p>Set difficulty:</p>
+		<button>8 tiles</button>
+		<button>16 tiles</button>
+		<button>24 tiles</button> */}
 	</header>
   )
 }
